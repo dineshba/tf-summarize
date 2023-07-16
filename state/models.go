9ed23425c82605492e1cc192028521d3e6038e6b
Dinesh B <dineshudt17@gmail.com>
2023-07-16 19:24:19 +0530

    Remove unused fields in the state
    Signed-off-by: Dinesh B <dineshudt17@gmail.com>

--- state/models.go
+++ state/models.go
@@ -3,13 +3,9 @@ package state
 import "encoding/json"
 
 type stateV4 struct {
-	Version          stateVersionV4           `json:"version"`
-	TerraformVersion string                   `json:"terraform_version"`
-	Serial           uint64                   `json:"serial"`
-	Lineage          string                   `json:"lineage"`
-	RootOutputs      map[string]outputStateV4 `json:"outputs"`
-	Resources        []resourceStateV4        `json:"resources"`
-	CheckResults     []checkResultsV4         `json:"check_results"`
+	Version     stateVersionV4           `json:"version"`
+	RootOutputs map[string]outputStateV4 `json:"outputs"`
+	Resources   []resourceStateV4        `json:"resources"`
 }
 
 type outputStateV4 struct {
@@ -19,43 +15,9 @@ type outputStateV4 struct {
 }
 
 type resourceStateV4 struct {
-	Module         string                  `json:"module,omitempty"`
-	Mode           string                  `json:"mode"`
-	Type           string                  `json:"type"`
-	Name           string                  `json:"name"`
-	EachMode       string                  `json:"each,omitempty"`
-	ProviderConfig string                  `json:"provider"`
-	Instances      []instanceObjectStateV4 `json:"instances"`
-}
-
-type instanceObjectStateV4 struct {
-	IndexKey interface{} `json:"index_key,omitempty"`
-	Status   string      `json:"status,omitempty"`
-	Deposed  string      `json:"deposed,omitempty"`
-
-	SchemaVersion           uint64            `json:"schema_version"`
-	AttributesRaw           json.RawMessage   `json:"attributes,omitempty"`
-	AttributesFlat          map[string]string `json:"attributes_flat,omitempty"`
-	AttributeSensitivePaths json.RawMessage   `json:"sensitive_attributes,omitempty"`
-
-	PrivateRaw []byte `json:"private,omitempty"`
-
-	Dependencies []string `json:"dependencies,omitempty"`
-
-	CreateBeforeDestroy bool `json:"create_before_destroy,omitempty"`
-}
-
-type checkResultsV4 struct {
-	ObjectKind string                 `json:"object_kind"`
-	ConfigAddr string                 `json:"config_addr"`
-	Status     string                 `json:"status"`
-	Objects    []checkResultsObjectV4 `json:"objects"`
-}
-
-type checkResultsObjectV4 struct {
-	ObjectAddr      string   `json:"object_addr"`
-	Status          string   `json:"status"`
-	FailureMessages []string `json:"failure_messages,omitempty"`
+	Module string `json:"module,omitempty"`
+	Type   string `json:"type"`
+	Name   string `json:"name"`
 }
 
 // stateVersionV4 is a weird special type we use to produce our hard-coded
